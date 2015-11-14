17b46d6a01a5d8bcbe6adb480296dbb4a30574e2
Philipp <philipp.kropik@gmail.com>
2015-11-14 15:27:37 +0100

    Auto

--- src/Car.java
+++ src/Car.java
@@ -2,7 +2,49 @@
 public class Car {
 
 	public int currentGear;
+	public int maxGear;
+	public int minGear;
 	public int currentSpeed;
 	public String colour;
 	public String brand;
+	public int year;
+	
+	public void nextGear()
+	{
+		if(currentGear < maxGear) //verhindert das nach dem letzten Gang weiter geschalten wird. 
+		{
+		currentGear++;
+		}
+	}
+	public void setCurrentGear(int newGear)
+	{
+		if (newGear <= maxGear && newGear >= minGear) // Stellt sicher das kein nicht zwischen Min und Max liegender Gang ausgewählt wird. 
+		{
+			currentGear = newGear;
+		}
+	}
+	public int getCurrentGear()
+	{
+		return currentGear;
+	}
+	
+	public void accelerate() //default Beschleunigung
+	{
+		currentSpeed += 10;
+	}
+	
+	public void accelerate(int customSpeed) // Variable Beschleunigung mit Übergabe Wert aus Main. 
+	{
+		currentSpeed += customSpeed;
+	}
+	
+	public void decelerate() //default Verzögerung
+	{
+		currentSpeed -= 10;
+	}
+	
+	public void decelerate(int customSpeed) //Variable Verzögerung mit Übergabe.  
+	{
+		currentSpeed -= customSpeed;
+	}
 }
---
+++ src/Demo.java
@@ -0,0 +1,44 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int n;
+		Car porsche911 = new Car();
+		porsche911.brand = "Porsche";
+		porsche911.colour = "Black";
+		porsche911.year = 1976;
+		porsche911.minGear = 0;
+		porsche911.maxGear = 6;
+		porsche911.setCurrentGear(4);
+	
+		
+		Car audiA3 = new Car();
+		audiA3.brand = "Audi";
+		audiA3.colour = "red";
+		audiA3.year = 2015;
+		audiA3.minGear = 0;
+		audiA3.maxGear = 5;
+		audiA3.setCurrentGear(3);
+		
+		System.out.println(porsche911.brand);
+		System.out.println("Aktuellere Gang -Porsche "+porsche911.currentGear);
+		System.out.println("Aktuellere Gang -Porsche "+audiA3.currentGear);
+		porsche911.nextGear();
+		System.out.println(porsche911.currentGear);
+		System.out.println("Aktuellere Gang -Porsche "+porsche911.currentGear);
+		System.out.println("Aktuellere Gang -Porsche "+audiA3.currentGear);
+		
+		porsche911.accelerate(10);
+		System.out.println("Speed: " + porsche911.currentSpeed);
+		porsche911.accelerate(2);
+		System.out.println("Speed: " + porsche911.currentSpeed);
+		porsche911.accelerate(22);
+		System.out.println("Speed: " + porsche911.currentSpeed);
+		porsche911.accelerate(100);
+		System.out.println("Speed: " + porsche911.currentSpeed);
+		porsche911.accelerate(20);
+		System.out.println("Speed: " + porsche911.currentSpeed);
+		}
+
+}
